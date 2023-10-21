65c51f92f29b6da3d308007a3ad3884b12479326
muralikunda <kundamails@gmail.com>
2023-10-20 21:11:48 -0400

    initial commit

---
+++ src/main/java/com/libraries/pages/Browser.java
@@ -0,0 +1,21 @@
+package com.libraries.pages;
+
+import com.Utilities.BasePage;
+import medha.MedhaLibrary.MedhaDataReader;
+import medha.MedhaLibrary.MedhaReporter;
+
+public class Browser extends BasePage {
+
+    public void openBrowser(){
+        initialize(MedhaDataReader.getBrowser());
+        String url = MedhaDataReader.getFieldValue("url");
+        try{
+            driver.get(url);
+            driver.manage().window().maximize();
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        MedhaReporter.report("Browser opened","info","");
+    }
+
+}
